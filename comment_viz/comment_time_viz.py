5cb49647ee0571b96c6da807ff07d5ea9df93530
czh <2834395095@qq.com>
2022-12-26 10:58:45 +0800

    viz

---
+++ comment_viz/comment_time_viz.py
@@ -0,0 +1,114 @@
+from matplotlib import pyplot as plt
+import pandas as pds
+import matplotlib
+from wordcloud import WordCloud, STOPWORDS
+import jieba
+def comment_time_viz(level_list = ['0','1','2','3','4','5','6'], main_path = './comment_data/main_data.csv',reply_path = './comment_data/reply_data.csv'):
+    
+    main_comment = pds.read_csv(main_path)
+    reply_comment = pds.read_csv(reply_path)
+    d = dict()
+    
+    for line in range(main_comment.shape[0]):
+        level = str(main_comment['等级'][line])
+        if level not in level_list :
+            continue
+        
+        comment_time = main_comment['发布时间'][line]
+        
+        if comment_time.find('天') != -1:
+            comment_time = float(comment_time.split('天')[0])
+        elif comment_time.find('小时') != -1 :
+            comment_time = float(comment_time.split('小时')[0])/24
+        elif comment_time.find('分钟') != -1 :
+            comment_time = float(comment_time.split('分钟')[0])/24/60
+            
+        if d.get(comment_time) is None:
+            d.update({comment_time:1})
+        else:
+            d.update({comment_time:d.get(comment_time)+1})
+            
+    for line in range(reply_comment.shape[0]):
+        level = str(reply_comment['等级'][line])
+        if level not in level_list :
+            continue
+        
+        comment_time = reply_comment['发布时间'][line]
+        
+        if comment_time.find('天') != -1:
+            comment_time = float(comment_time.split('天')[0])
+        elif comment_time.find('小时') != -1 :
+            comment_time = float(comment_time.split('小时')[0])/24
+        elif comment_time.find('分钟') != -1 :
+            comment_time = float(comment_time.split('分钟')[0])/24/60
+        
+        if d.get(comment_time) is None:
+            d.update({comment_time:1})
+        else:
+            d.update({comment_time:d.get(comment_time)+1})
+            
+    from collections import OrderedDict
+    d = OrderedDict(sorted(d.items()))
+    
+    fig = matplotlib.figure.Figure(figsize=(15, 5), dpi=75)
+    fig.add_subplot(111).plot(d.keys(),d.values())
+    
+    return fig
+
+def comment_level_viz(level_list = [] ,main_path = './comment_data/main_data.csv',reply_path = './comment_data/reply_data.csv'):
+    
+    main_comment = pds.read_csv(main_path)
+    reply_comment = pds.read_csv(reply_path)
+    d = dict()
+    for i in range(7) :
+        d.update({i:0})
+    
+    for line in range(main_comment.shape[0]):
+        level = main_comment['等级'][line]
+        d.update({level:d.get(level)+1})
+            
+    for line in range(reply_comment.shape[0]):
+        level = reply_comment['等级'][line]
+        d.update({level:d.get(level)+1})    
+        
+    select_list = [0] * len(d.values())
+    for l in level_list:
+        select_list[int(l)] = 0.1
+    select_list[1] = 0.8
+    from collections import OrderedDict
+    
+    d = OrderedDict(sorted(d.items()))
+    
+    labels = [ f'level{i}:{d[i]}' for i in d.keys()]
+    
+    fig = matplotlib.figure.Figure(figsize=(5, 5), dpi=75)
+    fig.add_subplot(111).pie(d.values(),labels = labels,explode=select_list,wedgeprops=dict(width=0.4, edgecolor='w'))
+    
+    return fig
+
+def word_cloud(main_path = './comment_data/main_data.csv',reply_path = './comment_data/reply_data.csv'):
+    main_comment = pds.read_csv(main_path)
+    reply_comment = pds.read_csv(reply_path)
+    content = ''
+    for line in range(main_comment.shape[0]):
+        
+        content += main_comment['评论内容'][line] + ' '
+        # d.update({level:d.get(level)+1})
+            
+    for line in range(reply_comment.shape[0]):
+        content += reply_comment['评论内容'][line] + ' '
+        # content = reply_comment['评论内容'][line]
+        # d.update({level:d.get(level)+1})
+    word_list = jieba.cut(content,cut_all=True)
+    cmt_word_split = (' '.join(word_list))
+    # import sys
+    # sys.stdout.reconfigure(encoding='utf-8')
+    # print(cmt_word_split)
+    cmt_wordcloud = WordCloud(background_color='#ffffff',font_path='./font/SmileySans-Oblique.ttf').generate(cmt_word_split)
+    
+    fig = matplotlib.figure.Figure(figsize=(9.9, 5), dpi=75)
+    fig.add_subplot(111).imshow(cmt_wordcloud)
+    fig.axes[0].get_xaxis().set_visible(False)
+    fig.axes[0].get_yaxis().set_visible(False)
+    
+    return fig
---
+++ main.py
@@ -0,0 +1,92 @@
+import PySimpleGUI as sg
+import os
+
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import PySimpleGUI as sg
+import matplotlib
+
+from comment_viz.comment_time_viz import *
+
+# fig = matplotlib.figure.Figure(figsize=(20, 4), dpi=50)
+# ax = fig.add_subplot(111)
+# ax = comment_time_viz(ax=ax,level_limit=6)
+matplotlib.use("TkAgg")
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="none", expand=1)
+    return figure_canvas_agg
+
+def delete_fig_agg(fig_agg):
+    fig_agg.get_tk_widget().forget()
+
+file_column = [
+                # [
+                #     sg.Text("Path:"),
+                #     sg.In(size = (5,1),enable_events = True,key = "-FOLDER-"),
+                #     sg.FolderBrowse(button_text='File',size=(5,1)),
+                # ],
+                # [
+                #     sg.Listbox(values=['e0','e1','e2'], enable_events=True, size=(17, 10), key="-FILE LIST-")
+                # ]
+                
+            ]
+viz_column = [
+                [
+                    sg.Text('user level:'),
+                    sg.In(default_text='0,1,2,3,4,5,6',size = (16,1),enable_events = True,key = "-LEVEL-"), 
+                    sg.VSeparator(),
+                    sg.Button("Viz",key="ok",tooltip=True),
+                    # sg.Check(text='<'),
+                    # sg.Check(text='='),
+                    # sg.Check(text='>'),
+                ],
+                [sg.Canvas(key="-Comment_Time_Viz-",size = (120,40))],
+                [sg.Canvas(key="-Comment_Level_Viz-",size = (40,40)),sg.Canvas(key="-Word_Cloud_Viz-",size = (60,40))],
+            ]
+
+
+
+layout = [[sg.Column(file_column),sg.VSeparator(),sg.Column(viz_column)]]
+
+window = sg.Window(title="BiliBili Comment Viz", layout=layout, background_color="#ffffff", font='Courier 12',finalize=True, element_justification='center')
+
+level_list = ['0','1','2','3','4','5','6']
+time_fig = comment_time_viz(level_list=level_list)
+time_fig_agg = draw_figure(window["-Comment_Time_Viz-"].TKCanvas, time_fig)
+
+level_fig = comment_level_viz()
+level_fig_agg = draw_figure(window["-Comment_Level_Viz-"].TKCanvas, level_fig)
+
+word_cloud_fig = word_cloud()
+word_cloud_fig_agg = draw_figure(window["-Word_Cloud_Viz-"].TKCanvas, word_cloud_fig)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED :
+            break
+    if event == "-FILE LIST-" :
+        print(values["-FILE LIST-"])
+    if event == "-LEVEL-" :
+        if values["-LEVEL-"] != '':
+            level_list = values["-LEVEL-"].split(',')
+            print(level_list)
+    if event == "ok":
+        delete_fig_agg(time_fig_agg)
+        time_fig = comment_time_viz(level_list=level_list)
+        time_fig_agg = draw_figure(window["-Comment_Time_Viz-"].TKCanvas, time_fig)
+        delete_fig_agg(level_fig_agg)
+        level_fig = comment_level_viz(level_list=level_list)
+        level_fig_agg = draw_figure(window["-Comment_Level_Viz-"].TKCanvas, level_fig)
+    
+    if event == "-FOLDER-" :
+        folder = values["-FOLDER-"]
+        try:
+            filelist = os.listdir(folder)
+            print(filelist)
+        except:
+            filelist = []
+            
+window.close()
